390e11a2b62455b8ecd4462aa970e56c9cd40499
safaamostafa <safamos21@gmail.com>
2025-01-14 23:22:39 +0200

    create configuration model

--- configuration/models.py
+++ configuration/models.py
@@ -6,3 +6,14 @@ class Slider(models.Model):
     image = models.ImageField(upload_to='media/')
     ordering = models.IntegerField()
     link = models.URLField(null=True)
+
+
+class Configuration(models.Model):
+    phone_number_one = models.CharField(max_length=50)
+    phone_number_two = models.CharField(max_length=50)
+    adderss_one = models.CharField(max_length=100)
+    adderss_two = models.CharField(max_length=100)
+    email = models.EmailField()
+    about_us = models.TextField()
+    Our_vision = models.TextField()
+    our_mission = models.TextField()
--- configuration/serializer.py
+++ configuration/serializer.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
 
-from configuration.models import Slider
+from configuration.models import Configuration, Slider
 
 
 class SliderSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Slider
+        fields = '__all__'
+
+
+class ConfigurationSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Configuration
         fields = '__all__'
