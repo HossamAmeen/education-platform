76e3a28aee0d8aabb3eef46a3aaada9d844cc650
safaamostafa <safamos21@gmail.com>
2025-01-15 14:38:04 +0200

    add social media fields

--- configuration/models.py
+++ configuration/models.py
@@ -9,11 +9,15 @@ class Slider(models.Model):
 
 
 class Configuration(models.Model):
-    phone_number_1 = models.CharField(max_length=50)
-    phone_number_2 = models.CharField(max_length=50)
+    phone_number_1 = models.CharField(max_length=15)
+    phone_number_2 = models.CharField(max_length=15)
     adderss_1 = models.CharField(max_length=100)
     adderss_2 = models.CharField(max_length=100)
     email = models.EmailField()
     about_us = models.TextField()
     our_vision = models.TextField()
     our_mission = models.TextField()
+    meta = models.URLField(null=True)
+    twitter = models.URLField(null=True)
+    linkedin = models.URLField(null=True)
+    googel = models.URLField(null=True)
